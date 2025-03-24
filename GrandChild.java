622e2218e1d9f99eae934231a59e7a661c68e3c8
abinaya368 <pabinayae@gmail.com>
2025-03-24 11:39:02 +0530

    Add files via upload

---
+++ GrandChild.java
@@ -0,0 +1,13 @@
+package session5;
+
+public class GrandChild extends child {
+	
+	public static void main(String[] args) {
+		GrandChild gc = new GrandChild();
+		gc.assets();
+		gc.vehicle();
+		
+	}
+	
+
+}
