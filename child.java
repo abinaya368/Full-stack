88021e1af788a01f0b3af764509c809f980f2b5e
abinaya368 <pabinayae@gmail.com>
2025-03-24 11:27:22 +0530

    Add files via upload

---
+++ child.java
@@ -0,0 +1,13 @@
+package session5;
+
+public class child extends Parent{
+	
+	public  void vehicle() {
+		System.out.println("Car");
+	}
+public static void main(String[] args) {
+	child cd = new child();
+	cd.assets();
+	cd.vehicle();
+}
+}
